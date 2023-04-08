98b59dbc7d1ba68e2c59a7b3a4f6caaccd9b2491
christian <32648607+crowdozer@users.noreply.github.com>
2023-04-08 03:15:50 -0500

    Update +server.ts

--- client/src/routes/api/feed/+server.ts
+++ client/src/routes/api/feed/+server.ts
@@ -3,7 +3,11 @@
  */
 export async function GET() {
 	console.log('loading rss');
-	const rss = await fetch('https://www.infosecurity-magazine.com/rss/news/');
+	const rss = await fetch('https://www.infosecurity-magazine.com/rss/news/', {
+		headers: {
+			'Accept-Encoding': 'identity'
+		}
+	});
 
 	console.log('sending rss to client ');
 	return rss;
