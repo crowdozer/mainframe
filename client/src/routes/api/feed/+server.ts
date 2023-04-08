a3bd3dfd09dda3f41d704254b1a06e3d4d320b2f
christian <32648607+crowdozer@users.noreply.github.com>
2023-04-08 03:17:01 -0500

    ...

--- client/src/routes/api/feed/+server.ts
+++ client/src/routes/api/feed/+server.ts
@@ -2,13 +2,11 @@
  * proxy fetch requests for the client
  */
 export async function GET() {
-	console.log('loading rss');
 	const rss = await fetch('https://www.infosecurity-magazine.com/rss/news/', {
 		headers: {
 			'Accept-Encoding': 'identity'
 		}
 	});
 
-	console.log('sending rss to client ');
 	return rss;
 }
