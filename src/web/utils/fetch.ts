babc6ca1f14e0b9a2496356e785363162f17e51b
christian <32648607+crowdozer@users.noreply.github.com>
2023-04-25 06:13:46 -0500

    Update fetch.ts

--- src/web/utils/fetch.ts
+++ src/web/utils/fetch.ts
@@ -4,8 +4,23 @@
  */
 export function enforceStatusCode<T extends Response>(response: T): T {
 	if (!response.ok) {
-		throw new Error(`[${response.status}] ${response.statusText}`);
+		const status = response.status;
+		const message = inferStatusMessage(status) || response.statusText || 'Unknown';
+
+		throw new Error(`[${response.status}] ${message}`);
 	}
 
 	return response;
 }
+
+export function inferStatusMessage(code: number): string | null {
+	return statusCodes[code] || null;
+}
+
+export const statusCodes: { [key: number]: string } = {
+	400: 'Bad Request',
+	401: 'Unauthorized',
+	404: 'Not Found',
+	420: 'Rate Limit',
+	500: 'Server Error'
+};
