53cfce6de4d0a3ee7032a3bb02d342b8c69613b9
christian <32648607+crowdozer@users.noreply.github.com>
2023-05-01 11:35:13 -0500

    Update +page.ts

--- src/routes/spotify/+page.ts
+++ src/routes/spotify/+page.ts
@@ -1,6 +1,5 @@
-import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load = async function () {
 	throw redirect(307, '/spotify/me');
-} satisfies PageServerLoad;
+};
