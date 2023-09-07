a6f2859704073c3887efde66cbdfe47eecc0fbaf
christian <32648607+crowdozer@users.noreply.github.com>
2023-09-07 17:08:31 -0500

    fixed janky card impl

---
+++ src/components/react/LuxortsDetails.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { Button } from '../shadcn/ui/button';
+
+export default function LuxortsDetails() {
+	const [more, setShowMore] = useState(false);
+
+	if (!more) {
+		return (
+			<>
+				<ul>
+					<li>
+						Designed and built web applications for sales and business
+						operations
+					</li>
+					<li>Enhanced UI/UX for customers</li>
+					<li>Improved UI/UX for internal tools</li>
+					<li>...</li>
+				</ul>
+				<br />
+				<Button onClick={() => setShowMore(true)} variant="outline">
+					show more
+				</Button>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<ul>
+				<li>
+					Designed and built web applications for sales and business operations
+				</li>
+				<li>Enhanced UI/UX for customers</li>
+				<li>Improved UI/UX for internal tools</li>
+
+				<li>Facilitated customer payments via Stripe Payments API</li>
+				<li>Reduced server loads by optimizing or rewriting legacy code</li>
+				<li>Created and maintained Docker images</li>
+				<li>
+					Configured and automated unit tests, end to end tests, integration
+					tests
+				</li>
+			</ul>
+			<br />
+			<Button onClick={() => setShowMore(false)} variant="outline">
+				show less
+			</Button>
+		</>
+	);
+}
