951aa6960b1c87e8c2bee3d894a27f3828feb967
christian <32648607+crowdozer@users.noreply.github.com>
2023-10-08 19:27:27 -0500

    update to luxorts card

--- src/components/react/portfolio/LuxortsDetails.tsx
+++ src/components/react/portfolio/LuxortsDetails.tsx
@@ -4,43 +4,68 @@ import { Button } from '@/components/react/ui'
 export default function LuxortsDetails() {
 	const [more, setShowMore] = useState(false)
 
-	if (!more) {
-		return (
-			<>
+	return (
+		<ul>
+			<li>
+				Launched the Luxorts web app, to provide direct market of Luxury Condos
+			</li>
+			<li style={{ listStyle: 'none' }}>
 				<ul>
 					<li>
-						Designed and built web applications for sales and business
-						operations
+						<a
+							href="https://www.luxorts.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							www.luxorts.com
+						</a>
 					</li>
-					<li>Enhanced UI/UX for customers</li>
-					<li>Improved UI/UX for internal tools</li>
-					<li>...</li>
 				</ul>
-				<br />
-				<Button onClick={() => setShowMore(true)}>show more</Button>
-			</>
-		)
-	}
-
-	return (
-		<>
-			<ul>
-				<li>
-					Designed and built web applications for sales and business operations
-				</li>
-				<li>Enhanced UI/UX for customers</li>
-				<li>Improved UI/UX for internal tools</li>
-
-				<li>Facilitated customer payments via Stripe Payments API</li>
-				<li>Reduced server loads by optimizing or rewriting legacy code</li>
-				<li>Created and maintained Docker images</li>
-				<li>
-					Configured and automated unit tests, end to end tests, integration
-					tests
-				</li>
-			</ul>
-			<br />
-			<Button onClick={() => setShowMore(false)}>show less</Button>
-		</>
+			</li>
+			<li>
+				Built and maintained in-house CMS systems with customized CRUD
+				workflows, wizards, tooling, and API integrations (CiiRUS, Stripe,
+				MailChimp)
+			</li>
+			<li>
+				Implemented dashboards, reports, and datatables to provide data insights
+			</li>
+			<li>
+				Integrated a secure Leads API to digest, sanitize, and distribute new
+				call center leads from both internal and external sources
+			</li>
+			<li>
+				Created internal tooling to automate repetitive tasks, reducing data
+				entry labor
+			</li>
+			{more && (
+				<>
+					<li>
+						Collaborated with leadership to identify software requirements
+					</li>
+					<li>Automated testing (PHPUnit + Jest)</li>
+					<li>
+						Optimized and refactored legacy servers or prototypes when necessary
+					</li>
+					<li>
+						Created and maintained Docker images to provide consistency and
+						reproducibility
+					</li>
+					<li>Created and maintained the various Github repos</li>
+					<li>Assisted in various hardware related problems</li>
+					<li style={{ listStyle: 'none', marginTop: '1rem' }}>
+						<Button onClick={() => setShowMore(false)}>show less</Button>
+					</li>
+				</>
+			)}
+			{!more && (
+				<>
+					<li>...</li>
+					<li style={{ listStyle: 'none', marginTop: '1rem' }}>
+						<Button onClick={() => setShowMore(true)}>show more</Button>
+					</li>
+				</>
+			)}
+		</ul>
 	)
 }
