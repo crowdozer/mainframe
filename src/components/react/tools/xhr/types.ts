4f175ccb9255999ebf787c942aa48bb62d31a00d
Christian Brown <crwdzr@gmail.com>
2024-01-12 10:19:25 -0600

    .

--- src/components/react/tools/xhr/types.ts
+++
@@ -1,9 +0,0 @@
-export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'OPTIONS'
-export type BodyType = 'json' | 'other'
-export type Header = {
-	name: string
-	value: string
-	hasError: boolean
-	hasWarning: boolean
-	id: string
-}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,30 +8,7 @@ module.exports = {
 		'./app/**/*.{ts,tsx}',
 		'./src/**/*.{ts,tsx}',
 	],
-	theme: {
-		container: {
-			center: true,
-			padding: '2rem',
-			screens: {
-				'2xl': '1400px',
-			},
-		},
-		extend: {
-			keyframes: {
-				'accordion-down': {
-					from: { height: 0 },
-					to: { height: 'var(--radix-accordion-content-height)' },
-				},
-				'accordion-up': {
-					from: { height: 'var(--radix-accordion-content-height)' },
-					to: { height: 0 },
-				},
-			},
-			animation: {
-				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out',
-			},
-		},
-	},
+	safelist: ['contents', 'hidden'],
+	theme: {},
 	plugins: [require('tailwindcss-animate')],
-};
+}
