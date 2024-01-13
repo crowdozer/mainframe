d2f7967afd9c3bea819949cc479d545d3eb1a350
crwdzr <crwdzr@gmail.com>
2024-01-13 16:51:45 -0600

    enable gzip/brotli

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
 		tailwind({
 			applyBaseStyles: false,
 		}),
-		react(),
+        compressor(),
+		// react(),
 	],
 });
