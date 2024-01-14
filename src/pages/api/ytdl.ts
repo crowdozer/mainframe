06b6d6e5e22da95abec9dcd285953b195dfec07f
Christian Brown <crwdzr@gmail.com>
2024-01-14 01:20:59 -0600

    disable downloader for now

--- src/pages/api/ytdl.ts
+++ src/pages/api/ytdl.ts
@@ -8,47 +8,48 @@ export const prerender = false
  * expects url to be set in formdata
  */
 export const POST: APIRoute = async ({ request }) => {
-	/**
-	 * get and validate the url
-	 */
-	const data = await request.formData()
-	const url = (data.get('url') || '') as string
-	if (!url || !ytdl.validateURL(url)) {
-		return new Response(JSON.stringify({ error: 'Invalid URL' }), {
-			status: 400,
-		})
-	}
+	return new Response(null, { status: 401 })
+	// /**
+	//  * get and validate the url
+	//  */
+	// const data = await request.formData()
+	// const url = (data.get('url') || '') as string
+	// if (!url || !ytdl.validateURL(url)) {
+	// 	return new Response(JSON.stringify({ error: 'Invalid URL' }), {
+	// 		status: 400,
+	// 	})
+	// }
 
-	/**
-	 * now begin to stream the video to the client
-	 */
-	try {
-		// it needs to be in this stream format
-		const stream = new ReadableStream({
-			start(controller) {
-				// Stream the video directly to the client
-				const videoStream = ytdl(url)
-				videoStream.on('data', (chunk) => {
-					controller.enqueue(chunk)
-				})
-				videoStream.on('end', () => {
-					controller.close()
-				})
-				videoStream.on('error', (error) => {
-					controller.error(error)
-				})
-			},
-		})
+	// /**
+	//  * now begin to stream the video to the client
+	//  */
+	// try {
+	// 	// it needs to be in this stream format
+	// 	const stream = new ReadableStream({
+	// 		start(controller) {
+	// 			// Stream the video directly to the client
+	// 			const videoStream = ytdl(url)
+	// 			videoStream.on('data', (chunk) => {
+	// 				controller.enqueue(chunk)
+	// 			})
+	// 			videoStream.on('end', () => {
+	// 				controller.close()
+	// 			})
+	// 			videoStream.on('error', (error) => {
+	// 				controller.error(error)
+	// 			})
+	// 		},
+	// 	})
 
-		return new Response(stream, {
-			headers: {
-				'Content-Type': 'video/mp4',
-				'Content-Disposition': 'attachment; filename="download.mp4"',
-			},
-		})
-	} catch (error) {
-		return new Response(JSON.stringify({ error: 'Error downloading video' }), {
-			status: 500,
-		})
-	}
+	// 	return new Response(stream, {
+	// 		headers: {
+	// 			'Content-Type': 'video/mp4',
+	// 			'Content-Disposition': 'attachment; filename="download.mp4"',
+	// 		},
+	// 	})
+	// } catch (error) {
+	// 	return new Response(JSON.stringify({ error: 'Error downloading video' }), {
+	// 		status: 500,
+	// 	})
+	// }
 }
