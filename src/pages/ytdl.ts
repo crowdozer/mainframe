cc92c96ef9e2a34447daaeb672a0b6e5dcbcb54b
Christian Brown <crwdzr@gmail.com>
2024-01-14 00:07:43 -0600

    Update ytdl.ts

--- src/pages/ytdl.ts
+++ src/pages/ytdl.ts
@@ -1,49 +1,44 @@
 import type { APIRoute } from 'astro'
-import fs from 'fs'
 import ytdl from 'ytdl-core'
-import crypto from 'crypto'
 
-const sleep = (ms: number) => {
-	return new Promise<void>((resolve) => {
-		setTimeout(() => {
-			resolve()
-		}, ms)
-	})
-}
-
-export const GET: APIRoute = async () => {
-	const url = 'https://www.youtube.com/watch?v=a5ITNmnS680'
-	const id = crypto.randomUUID()
-	const filename = `${id}.mp4`
-	const target = `./public/downloads/${filename}`
-
-	let isRunning = true
-
-	console.log('downloading %s', url)
-	ytdl(url)
-		.pipe(fs.createWriteStream(target))
-		.on('finish', () => {
-			console.log('Download completed!')
-			isRunning = false
+export const GET: APIRoute = async ({ request }) => {
+	const url = new URL(request.url).searchParams.get('url')
+	if (!url || !ytdl.validateURL(url)) {
+		return new Response(JSON.stringify({ error: 'Invalid URL' }), {
+			status: 400,
 		})
-
-	while (isRunning) {
-		console.log('still downloading...')
-		await sleep(5000)
 	}
 
-	console.log('download complete!')
-
-	return new Response(
-		JSON.stringify({
-			message: 'your file is ready',
-			time: new Date(),
-		}),
-		{
-			status: 200,
-			headers: {
-				'Content-Disposition': `attachment; filename="${target}"`,
+	try {
+		// it needs to be in this stream format
+		const stream = new ReadableStream({
+			start(controller) {
+				// Stream the video directly to the client
+				const videoStream = ytdl(url)
+				videoStream.on('data', (chunk) => {
+					controller.enqueue(chunk)
+				})
+				videoStream.on('end', () => {
+					controller.close()
+				})
+				videoStream.on('error', (error) => {
+					controller.error(error)
+				})
 			},
-		},
-	)
+			cancel() {
+				console.log('cancelled')
+			},
+		})
+
+		return new Response(stream, {
+			headers: {
+				'Content-Type': 'video/mp4',
+				'Content-Disposition': 'attachment; filename="download.mp4"',
+			},
+		})
+	} catch (error) {
+		return new Response(JSON.stringify({ error: 'Error downloading video' }), {
+			status: 500,
+		})
+	}
 }
