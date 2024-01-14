f34597a18ea1d29dbe84c440bdc9aef73048eef2
Christian Brown <crwdzr@gmail.com>
2024-01-14 00:32:22 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 // import react from '@astrojs/react';
 import compressor from 'astro-compressor'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'hybrid',
+	adapter: vercel(),
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
